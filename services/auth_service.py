5928fcc04c33f24c2ad7544fdca177aed49f1708
samuvel <samuvelraja072gmail.com>
2026-06-11 23:16:28 +0530

    create user changes

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,14 +1,15 @@
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from config import settings
 from fastapi import Request
-
-
+from fastapi.responses import JSONResponse
+from services.user_service import User_service
 
 class Auth_Service:
     def __init__(self):
         self.google_client_id=settings.google_client_id
         self.google_client_secret=settings.google_client_secret
         self.oauth = OAuth()
+        self.user_service = User_service()
         self.oauth.register(
             name='google',
             server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
@@ -21,7 +22,12 @@ class Auth_Service:
         )
 
     async def google_login_service(self,request,url):
-        return await self.oauth.google.authorize_redirect(request, url,access_type="offline", prompt="consent")
+        try:
+            return await self.oauth.google.authorize_redirect(request, url,access_type="offline", prompt="consent")
+        except OAuthError as e:
+            return {
+                "message":e
+            }
         
 
     async def google_callback_service(self,request):
@@ -34,6 +40,7 @@ class Auth_Service:
         user = token.get('userinfo')
         if user:
             request.session['user'] = dict(user)
+            await self.user_service.create_user_service(user)
         return {
             "user":token
         }
--- services/user_service.py
+++ services/user_service.py
@@ -1,5 +1,24 @@
 from authlib.integrations.starlette_client import OAuth, OAuthError
+from models.user_model import User
+from fastapi.responses import JSONResponse
 
 class User_service:
-   def user():
-      pass
+   async def create_user_service(self, user: dict):
+      try:
+         existing_user = User.objects(email=user.get("email"))
+         if existing_user:
+            return JSONResponse({
+               "status": 400,
+               "message": "User already exists"
+            })
+         user = User(**user)
+         user.save()
+         return JSONResponse({
+            "status": 201,
+            "message": "User created successfully"
+         })
+      except Exception as e:
+         return JSONResponse({
+            "status": 500,
+            "message": str(e)
+         })
